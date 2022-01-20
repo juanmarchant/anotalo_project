d76992634bdb3389a9be1ab05080954c0177a526
JuanMarchant <juanmarchantdev@gmail.com>
2022-01-19 21:40:58 -0300

    Update meta tags and create more blocks

--- anotalo_project/anotalo_project/settings.py
+++ anotalo_project/anotalo_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'www.anotalo.website']
 
 
 # Application definition
